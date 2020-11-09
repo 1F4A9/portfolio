813036d6873975c2374f61bdf075fef0fb4c9d9d
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-11-09 14:26:54 +0100

    changed svg icons

--- src/components/views/contact/about/About.js
+++ src/components/views/contact/about/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FaUserGraduate, FaCheckSquare, FaHandPeace } from 'react-icons/fa';
+import { FaUserGraduate, FaCheckSquare, FaHandPeace, FaHeart, FaGlasses } from 'react-icons/fa';
 
 import CenterContent from './CenterContent';
 import ParagraphContainer from './ParagraphContainer';
@@ -31,9 +31,9 @@ export default function About() {
   const paragraphs = [
     { icon: <FaHandPeace />, paragraph: `Frontend student` },
     { icon: <FaUserGraduate />, paragraph: `Graduates in ${calcDiffInTimeWith('2021-05-28', 'days')} days` },
-    { icon: <FaCheckSquare />, paragraph: `Problem solver` },
-    { icon: <FaCheckSquare />, paragraph: `Driven` },
-    { icon: <FaCheckSquare />, paragraph: `Eager to learn` },
+    { icon: <FaHeart />, paragraph: `Love of learning` },
+    { icon: <FaGlasses />, paragraph: `Problem solver` },
+    { icon: <FaCheckSquare />, paragraph: `Creative` },
   ]
 
   return (

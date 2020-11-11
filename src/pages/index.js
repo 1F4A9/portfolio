bfd8e08acf795aa8e905755243db793ebae3073b
Emil Asus Zenbook <emil.ahlm46@gmail.com>
2020-11-11 21:26:45 +0100

    react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Helmet } from 'react-helmet';
 
 import Nav from '../components/nav/Nav';
 import Intro from '../components/views/intro/Intro';
@@ -20,6 +21,10 @@ const Container = styled.div`
 export default function Index() {
   return (
     <Container>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Portfolio</title>
+      </Helmet>
       <Normalize />
       <GlobalStyle />
       <Typography />
